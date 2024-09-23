0fef76aafeff4b4452f9cc9fe6871753c35da17a
Ziad Ramzy <ziadramzy6@gmail.com>
2024-09-23 18:15:14 +0300

    Adding the first layer

---
+++ layers.py
@@ -0,0 +1,19 @@
+import torch
+import numpy
+import torch.nn as nn
+
+
+class Dense(nn.Module):
+    def __init__(self, input: int, output: int):
+        super().__init__()
+        self.x = torch.nn.Parameter(torch.randn(input, output))
+        self.b = torch.nn.Parameter(torch.zeros(output))
+
+    def forward(self, A: torch.tensor):
+
+        if A.shape[1] != self.x.shape[0]:
+            raise RuntimeError(f"Shape mismatch: Input shape {
+                               A.shape} can't broadcast with the weights {self.x.shape}")
+
+        y = A @ self.x + self.b
+        return y
